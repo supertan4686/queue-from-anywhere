3071384cd0a185518c74395da3cddc7a112a51f2
supertan4686 <supertan7381@gmail.com>
2018-06-29 01:15:53 +0700

    add query employee data

--- application/models/Employee_model.php
+++ application/models/Employee_model.php
@@ -5,6 +5,31 @@ class Employee_model extends CI_Model {
 		parent::__construct();
 		$this->load->database();
 	}
+
+	function get_employee_data($date){
+		//Sub query
+		$selectsub = "login_log.employee_id, min(login_log.login_time), max(login_log.logout_time)";
+		$this->db->select($selectsub);
+		$this->db->where('login_log.date', $date);
+		$this->db->group_by("login_log.employee_id");
+		$subquery = $this->db->get_compiled_select('login_log', FALSE);
+		$this->db->reset_query();
+
+		//Main query
+		$selectmain = "employee.employee_id, CONCAT(employee.employee_name_title, ' ', employee.employee_firstname, ' ', employee.employee_lastname) AS 'employee_name', count(queue_log.score) AS 'Amount customer', SUM(queue_log.score) AS 'Total Score', SUM(CASE queue_log.score WHEN '0' THEN 1 ELSE 0 END) AS '0', SUM(CASE queue_log.score WHEN '1' THEN 1 ELSE 0 END) AS '1', SUM(CASE queue_log.score WHEN '2' THEN 1 ELSE 0 END) AS '2', SUM(CASE queue_log.score WHEN '3' THEN 1 ELSE 0 END) AS '3', SUM(CASE queue_log.score WHEN '4' THEN 1 ELSE 0 END) AS '4', SUM(CASE queue_log.score WHEN '5' THEN 1 ELSE 0 END) AS '5'";
+		$this->db->select($selectmain);
+		$this->db->from('queue_log');
+		//Join subquery
+		$this->db->join(' (' . $subquery . ') AS login_log', 'login_log.employee_id = queue_log.employee_id');
+		//Join employee
+		$this->db->join('employee', 'employee.employee_id = queue_log.employee_id');
+		$this->db->group_by("queue_log.employee_id");
+
+		//Get query result
+		$result = $this->db->get()->result_array();
+		return $result;
+
+	}
 }
 
 ?>
